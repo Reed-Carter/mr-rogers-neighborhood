2970e81e6611c6c958aa215096a7d0688f5025fa
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-28 07:47:07 -0800

    create html with a user input field and a submit button and function passes first test to return a numbered array counting from 0 to the user inputted number

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 //Business Logic
-
+function mrRogers (number) {
+  let numberArray = [0]
+  for (let i = 0; i < number; i += 1) {
+    numberArray.push(numberArray[i] + 1);
+  };
+  return numberArray;
+}
 
 
 
