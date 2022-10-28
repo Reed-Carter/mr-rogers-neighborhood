c3882646d0a99b12141fb2b6331bb0705efca3d9
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-28 14:29:26 -0800

    add spaces between the elements of the aray

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function mrRogers (inputNumber) {
 
   let stringArray = []
   for (const element of numberArray) {
-    const stringNumber = element.toString(" ");
+    const stringNumber = element.toString();
     if (stringNumber.includes("3")) {
       stringArray.push("Won't you be my neighbor");
     }
@@ -21,7 +21,7 @@ function mrRogers (inputNumber) {
       stringArray.push(stringNumber);
     }
   }
- return stringArray;
+  return stringArray;
 }
 
 
@@ -31,7 +31,7 @@ function mrRogers (inputNumber) {
 function handleForm(event) {
   event.preventDefault();
   const inputNumber = document.getElementById("userInput").value;
-  let outputText = mrRogers(inputNumber);
+  let outputText = mrRogers(inputNumber).join(', ');
   document.getElementById("output").append(outputText);
   document.getElementById("output").append(document.createElement("br"));
 
